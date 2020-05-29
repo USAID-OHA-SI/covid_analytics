09813b4cc739032c34515a125c28eb4b5d621421
Baboyma Kagniniwa <baboyma@gmail.com>
2020-05-29 16:43:39 -0400

    Stream lining hex bins creation

--- Scripts/01_Extract_Geodata.R
+++ Scripts/01_Extract_Geodata.R
@@ -22,6 +22,16 @@ dir_gis <- "GIS"
 
 # FUNCTION ----------------------------------------------------------------
   
+  #' Get geospatial data from GADM
+  #' 
+  #' @param country_name ISO3 code
+  #' @param adm_level country administrative boundaries level
+  #' @param local save a copy?
+  #' @return geodata of type sp
+  #' @example 
+  #' get_geodata("UGA") 
+  #' get_geodata("UGA", adm_level=2, local=FALSE)
+  #' 
   get_geodata <- function(country_name, adm_level=0, local=TRUE) {
     
     # Read data form local dir first
@@ -30,8 +40,8 @@ dir_gis <- "GIS"
     # geodata location
     path_gis = here()
     
-    if (dir.exists(here("gis"))) {
-      path_gis <- here("gis")
+    if (dir.exists(here("GIS"))) {
+      path_gis <- here("GIS")
     }
     
     # Extract data from GADM
@@ -39,7 +49,7 @@ dir_gis <- "GIS"
                         country = country_name, 
                         level = adm_level,
                         download = downlaod,
-                        path = dir_gis)
+                        path = path_gis)
     
     # Force a download
     if (is.null(geo_data)) {
@@ -53,6 +63,53 @@ dir_gis <- "GIS"
     # Return formatted data
     return(geo_data)
   }
+
+  
+  #' Create an hexbins polygon feature class
+  #' 
+  #' @param country_name iso3 code
+  #' @param level country administrative unit level
+  #' @param size size of each hex bin in meters
+  #' @return country hex polygon as feature class
+  #' @example 
+  #' create_hexbins('LSO', 1)
+  #' create_hexbins('UGA', 1)
+  create_hexbins <- function(country_name, level=0, size=NULL) {
+    
+    cntry_adm <- get_geodata(country_name, adm_level=level) %>% 
+      st_transform(crs = st_crs(3857))
+    
+    cntry_adm0 <- cntry_adm
+    
+    if ( level != 0 ) {
+      cntry_adm0 <- cntry_adm %>% 
+        mutate(
+          id = row_number(),
+          area = st_area(.)
+        ) %>% 
+        group_by(id) %>% 
+        summarise(area = sum(area))
+    }
+    
+    cntry_hex <- cntry_adm0 %>% 
+      st_make_grid(what = "polygones", square = FALSE, n = c(10, 20)) %>% 
+      st_intersection(cntry_adm0)
+    
+    viz <- cntry_hex %>% 
+      ggplot() +
+      geom_sf(fill=NA, lwd=.3, color=grey30k) +
+      geom_sf(data=cntry_adm, fill=NA, lwd=.5, color=grey50k) +
+      labs(title = country_name, caption = "OHA/SIEI - Data Source: GADM") +
+      theme_void()
+    
+    print(viz)
+    
+    return(cntry_hex)
+  }
+  
+  # Test
+  #uga_hex <- create_hexbins('UGA', 1)
+  #lso_hex <- create_hexbins('LSO', 1)
 
 # DATA -------------------------------------------------------
 
